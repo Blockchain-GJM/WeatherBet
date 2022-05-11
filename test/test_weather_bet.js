7b3ff4cd3ea3b827e964e2ce61bb583f2fa593a2
Jacky Wong <hw2813@columbia.edu>
2022-05-10 21:18:39 -0400

    added architecture files for smart contract

---
+++ test/test_weather_bet.js
@@ -0,0 +1,13 @@
+const TestWeatherBet = artifacts.require("TestWeatherBet");
+
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+contract("TestWeatherBet", function (/* accounts */) {
+  it("should assert true", async function () {
+    await TestWeatherBet.deployed();
+    return assert.isTrue(true);
+  });
+});

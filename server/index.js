f14e9a3c6ffe8b8e29f27310256d50fab220dffa
geo-lit <geolit111999@gmail.com>
2022-05-11 11:34:55 -0400

    Weather check done in server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 //const mongoose = require('mongoose')
 const cors = require('cors')
+const schedule = require('node-schedule-tz')
+const fetch = require('node-fetch');
 require('dotenv/config')
 
 
@@ -47,3 +49,89 @@ app.post('/bet', async (req, res) => {
 app.listen(3001, ()=>{
     console.log("Server running on port 3001")
 });
+
+// Scheduler Ruleset
+const rule = new schedule.RecurrenceRule();
+rule.hour = 23;
+rule.minute = 59;
+rule.tz = 'America/New_York'
+
+// Test rule, run every minute when seconds = 4
+const ruleTest = new schedule.RecurrenceRule();
+ruleTest.second = 4
+
+const job = schedule.scheduleJob(rule, function(){
+    doPayouts();
+});
+
+
+function getDateStrings(){
+    let now = new Date(Date.parse((new Date()).toUTCString()))
+    
+
+    // test for May 6-7
+    // now.setDate(6)
+    
+    let upperBound = new Date(now)
+    // upperbound is New York 11:59pm
+    upperBound.setUTCHours(3)
+    upperBound.setUTCMinutes(59)
+    upperBound.setUTCSeconds(59)
+    upperBound.setUTCDate(upperBound.getUTCDate()+1)
+
+    // lowerbound is New York 5:50am to get 5:51am weather report for past 6 hour rain
+    let lowerBound = new Date(now)
+    lowerBound.setUTCHours(9)
+    lowerBound.setUTCMinutes(50)
+    upperBound.setUTCSeconds(0)
+
+    return [lowerBound, upperBound]
+}
+
+function doPayouts(){
+    console.log("======= RUNNING PAYOUT SCRIPT =======")
+    
+    var lower
+    var upper
+    [lower, upper] = getDateStrings();
+    
+    
+    let rainTypes = ["drizzle", "hail", "snow_pellets", 
+                    "ice_crystals", "ice_pellets",
+                    "rain", "snow_grains", "snow"]
+    
+    console.log(lower)
+    console.log(upper)
+    allTimeStamps = []
+
+    let url = "https://api.weather.gov/stations/KNYC/observations"
+    let settings = {method: "Get"}
+
+    let rainStatus = fetch(url, settings)
+        .then(res => res.json())
+        .then((json) => {
+            let wasRain = false
+            for (const item of json.features){
+                checkDate = Date.parse(item.properties.timestamp)
+                if (lower.getTime() <= checkDate && checkDate <= upper.getTime()){
+                    for (const description of item.properties.presentWeather){
+                        for (const rain of rainTypes){
+                            if (description.weather.includes(rain)){
+                                console.log(item.properties.timestamp, ": found rain")
+                                wasRain = true
+                            }
+                        }
+                    }
+                }
+            }
+            return wasRain
+        })
+        .then(rainStatus => {
+            console.log(rainStatus)
+            // PAYOUT INIT GOES HERE
+        })
+        .catch(error => console.error(error));
+    
+
+    return rainStatus
+};

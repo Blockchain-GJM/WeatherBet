2bd72f0829daf002cd3eba1e8765c0fb69d5eee7
Jacky Wong <hw2813@columbia.edu>
2022-05-10 21:43:44 -0400

    updated test

--- test/test_weather_bet.js
+++ test/test_weather_bet.js
@@ -1,11 +1,11 @@
-const TestWeatherBet = artifacts.require("TestWeatherBet");
+const TestWeatherBet = artifacts.require("WeatherBet");
 
 /*
  * uncomment accounts to access the test accounts made available by the
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("TestWeatherBet", function (/* accounts */) {
+contract("WeatherBet", function (/* accounts */) {
   it("should assert true", async function () {
     await TestWeatherBet.deployed();
     return assert.isTrue(true);
